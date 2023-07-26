dece13fbaccdf35d04df9f8927399b75183b44aa
DylanMetro <133925790+DylanMetro@users.noreply.github.com>
2023-07-26 14:28:43 -0500

    Add files via upload
    Created help.php page, and added Help button to header.php

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 ?>
-<link rel="icon" href="/images/npo-favicon.png" type="image/x-icon">
 <header data-role="Header" class="home-header">
   <a href="index.php"><img alt="image" src="./images/non-profit-portal-logo.png" loading="eager" class="home-image" /></a>
   <a href="index.php" rel="noreferrer noopener" class="home-link">
@@ -11,8 +10,10 @@ session_start();
     <span class="home-text">NPO List View</span>
     <br />
   </a>
-  <a href="reports.php" rel="noreferrer noopener" class="home-link">
-    Reports
+
+  <a href="help.php" rel="noreferrer noopener" class="home-link1">
+    <span class="home-text">Help</span>
+    <br />
   </a>
   
 
